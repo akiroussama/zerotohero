dfced94d62a1d3b0cf3747b95e9d393c50fa6a8a
Akir Oussama <akir.oussama@gmail.com>
2022-08-19 02:51:02 +0200

    add quote list

--- front/src/components/Welcome.tsx
+++ front/src/components/Welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import QuotesList from "./QuotesList";
 
 interface WelcomeProps {
   username: string;
@@ -10,6 +11,7 @@ const Welcome: React.FC<WelcomeProps> = ({username, onLogout}) => {
   return (
     <div className="max-w-screen-sm p-12 mx-auto bg-gray-50 rounded-md shadow-lg">
       <h1 className="text-2xl">Welcome {username}!</h1>
+      <QuotesList></QuotesList>
       <Button className='mt-2' onClick={onLogout}>Log Out</Button>
     </div>
   );
