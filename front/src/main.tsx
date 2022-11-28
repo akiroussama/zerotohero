256349f08b49747b7339a2a0ee842c93b5989d9b
oakir <oakir@ecomundo.eu>
2022-11-28 13:29:26 +0100

    fix msw issue

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { worker } from './mocks';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 // Start a mock API server to handle auth requests
-worker.start({
-  onUnhandledRequest: 'bypass'
-});
+// worker.start({
+//   onUnhandledRequest: 'bypass'
+// });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
